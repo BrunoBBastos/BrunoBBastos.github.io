08d2d0f4687c00cd8c1325241be42eaaa3132fcf
Bruno Bezerra Bastos <brn.suncity@gmail.com>
2021-12-16 02:43:12 -0300

    correções

--- IOT_Projeto/dash.js
+++ IOT_Projeto/dash.js
@@ -15,7 +15,7 @@ function sendToServer(state)
 	//criar um objeto capaz de enviar dados via requisição HTTP GET
 	const http = new XMLHttpRequest();
 	//prepara um GET passando a váriavel lux como ultimo parâmetro do link
-	http.open("GET", "https://api.thingspeak.com/update?api_key="+ KEY +" &field1 =0"+state);
+	http.open("GET", "https://api.thingspeak.com/update?api_key=" + KEY + "&field1=0"+state);
 
 	//envia um GET
 	http.send();
